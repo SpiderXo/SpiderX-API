33894ba35c9dfa8df1f83958ca9d786d405011dc
SpiderXo <zerxkillyou@gmail.com>
2026-01-13 19:12:08 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,41 +1,17 @@
-import jwt from "jsonwebtoken";
-
-const USERS = [
-  {
-    id: 1,
-    username: "SpiderX",
-    password: "123456",
-    apiKey: "SPIDERX-KEY-123456",
-    plan: "free",
-    requests: 0
-  }
-];
-
-const JWT_SECRET = "SPIDERX_SECRET";
-
 export default function handler(req, res) {
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
   const { username, password } = req.body;
 
-  const user = USERS.find(
-    u => u.username === username && u.password === password
-  );
-
-  if (!user) {
-    return res.status(401).json({ error: "Invalid credentials" });
+  if (username === "SpiderX" && password === "123456") {
+    return res.json({
+      token: "JWT_FAKE_TOKEN",
+      apiKey: "SPIDERX-KEY-123456"
+    });
   }
 
-  const token = jwt.sign(
-    { id: user.id, username: user.username },
-    JWT_SECRET,
-    { expiresIn: "1d" }
-  );
-
-  res.status(200).json({
-    token,
-    apiKey: user.apiKey
-  });
+  return res.status(401).json({ error: "Invalid login" });
 }
