14001a232bdaa3b45a7b2db16c2218a93e927386
SpiderXo <zerxkillyou@gmail.com>
2026-01-13 18:39:06 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,24 +1,11 @@
-import jwt from "jsonwebtoken";
-import { db } from "../database/db.js";
-
-const SECRET = "SPIDERX_SECRET";
-
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const { username, password } = req.body;
 
-  const user = await db.get(
-    "SELECT * FROM users WHERE username = ?",
-    username
-  );
+  if (username === "spiderx" && password === "123456") {
+    return res.json({
+      apiKey: "SPIDERX-KEY-123"
+    });
+  }
 
-  if (!user || password !== "123456")
-    return res.status(401).json({ error: "Invalid" });
-
-  const token = jwt.sign(
-    { id: user.id, username: user.username },
-    SECRET,
-    { expiresIn: "1d" }
-  );
-
-  res.json({ token });
+  res.status(401).json({ error: "Invalid login" });
 }
