c1a638b5c97ef6cb5b6349f6ac15e34b722cd851
SpiderXo <zerxkillyou@gmail.com>
2026-01-13 19:20:12 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,17 +1,18 @@
 export default function handler(req, res) {
-
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
   const { username, password } = req.body;
 
-  if (username === "SpiderX" && password === "123456") {
+  if (username === "SpiderX" && password === "1234") {
+    const token = "TOKEN-" + Date.now();
+
     return res.json({
-      token: "JWT_FAKE_TOKEN",
+      token,
       apiKey: "SPIDERX-KEY-123456"
     });
   }
 
-  return res.status(401).json({ error: "Invalid login" });
+  res.status(401).json({ error: "Invalid credentials" });
 }
