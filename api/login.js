e41b6b017aac9f5b8e6dde88ae76193c0476b032
SpiderXo <zerxkillyou@gmail.com>
2026-01-13 18:40:59 +0200

    Implement JWT authentication for login

--- api/login.js
+++ api/login.js
@@ -1,11 +1,27 @@
+import jwt from "jsonwebtoken";
+import { users } from "../users.js";
+
+const SECRET = "SPIDERX_SECRET";
+
 export default function handler(req, res) {
   const { username, password } = req.body;
 
-  if (username === "spiderx" && password === "123456") {
-    return res.json({
-      apiKey: "SPIDERX-KEY-123"
-    });
+  const user = users.find(
+    u => u.username === username && u.password === password
+  );
+
+  if (!user) {
+    return res.status(401).json({ error: "Invalid login" });
   }
 
-  res.status(401).json({ error: "Invalid login" });
+  const token = jwt.sign(
+    { id: user.id, role: user.role },
+    SECRET,
+    { expiresIn: "1d" }
+  );
+
+  res.json({
+    token,
+    apiKey: user.apiKey
+  });
 }
