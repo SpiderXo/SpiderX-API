85883a777163992fd8376656a3547fd7d75fc85c
SpiderXo <zerxkillyou@gmail.com>
2026-01-15 20:31:14 +0200

    Update me.js

--- api/users/me.js
+++ api/users/me.js
@@ -1,1 +1,11 @@
+import { db } from "../../core/db.js";
 
+export function me(req, res) {
+  db.get(
+    "SELECT username, apiKey, plan, requests FROM users WHERE id = ?",
+    [req.user.id],
+    (err, user) => {
+      res.json(user);
+    }
+  );
+}
