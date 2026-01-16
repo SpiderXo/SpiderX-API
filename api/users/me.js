fd505b21e3b44cbe16498ed66856a5312ac6cc29
SpiderXo <zerxkillyou@gmail.com>
2026-01-16 19:33:01 +0200

    Update me.js

--- api/users/me.js
+++ api/users/me.js
@@ -1,11 +1,26 @@
-import { db } from "../../core/db.js";
+import { verifyToken } from "../../core/auth.js";
+import { users } from "../../core/db.js";
 
-export function me(req, res) {
-  db.get(
-    "SELECT username, apiKey, plan, requests FROM users WHERE id = ?",
-    [req.user.id],
-    (err, user) => {
-      res.json(user);
-    }
-  );
+export default function handler(req, res) {
+  const auth = req.headers.authorization;
+
+  if (!auth) {
+    return res.status(401).json({ error: "No token" });
+  }
+
+  const token = auth.split(" ")[1];
+  const data = verifyToken(token);
+
+  if (!data) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+
+  const user = users.find(u => u.id === data.id);
+
+  return res.json({
+    username: user.username,
+    apiKey: user.apiKey,
+    plan: user.plan,
+    requests: user.requests
+  });
 }
