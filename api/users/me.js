3ba1090c9a6731985367aa4c13e6c8cd0144b24c
SpiderXo <zerxkillyou@gmail.com>
2026-01-16 20:14:44 +0200

    Update me.js

--- api/users/me.js
+++ api/users/me.js
@@ -1,26 +1,8 @@
-import { verifyToken } from "../../core/auth.js";
-import { users } from "../../core/db.js";
+import { verify } from "../../core/auth.js";
 
 export default function handler(req, res) {
-  const auth = req.headers.authorization;
+  const user = verify(req);
+  if (!user) return res.status(401).end();
 
-  if (!auth) {
-    return res.status(401).json({ error: "No token" });
-  }
-
-  const token = auth.split(" ")[1];
-  const data = verifyToken(token);
-
-  if (!data) {
-    return res.status(401).json({ error: "Invalid token" });
-  }
-
-  const user = users.find(u => u.id === data.id);
-
-  return res.json({
-    username: user.username,
-    apiKey: user.apiKey,
-    plan: user.plan,
-    requests: user.requests
-  });
+  res.json(user);
 }
